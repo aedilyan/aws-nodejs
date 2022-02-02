2a2cec730c8b8bb38e31200a18bd71e73acca25b
aedilyan <artur-edilyan@live.com>
2022-02-02 13:44:25 +0400

    deploy

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+const path = require('path')
 const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("hello from server");
-})
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
 
-app.listen(process.env.port || 3000, () => {
-    console.log('server started at port 3000');
+// app.get('/', (req, res) => {
+//     res.send("hello from server");
+// })
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log('server started');
 })
